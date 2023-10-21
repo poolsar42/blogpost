30ee46874a3ae3eebf7620e0ccd701a08448c4c3
emil <33105890+poolsar42@users.noreply.github.com>
2023-10-21 15:16:56 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="p-strip--dark" style={{ background: "#111" }}>
+        <div class="row">
+          <div class="col-12">
+            <p style={{ color: "#fff" }}>
+              The quick brown <code>fox&nbsp;jumps</code> over the lazy dog
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
