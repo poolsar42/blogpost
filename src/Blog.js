e60f7a16d8c17835d565affbb571b8ed4b04a999
emil <33105890+poolsar42@users.noreply.github.com>
2023-10-29 23:54:08 +0100

    fix: remove unnecessary code

--- src/Blog.js
+++ src/Blog.js
@@ -51,34 +51,6 @@ function Blog() {
             />
           );
         })}
-        {posts.map((post) => {
-          return (
-            <Post
-              key={post.id}
-              media={post.featured_media}
-              link={post.link}
-              title={post.title.rendered}
-              author={post._embedded.author[0].name}
-              authorLink={post._embedded.author[0].link}
-              date={new Date(post.date).toLocaleDateString("en-GB", {
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-              })}
-              type={
-                // In the returen API object I didn't find any type property
-                // So I manually added an Article type property
-                "Article"
-              }
-              category={
-                // In the returen API object I didn't find a better way to get the category
-                post._embedded["wp:term"][2].length
-                  ? post._embedded["wp:term"][2][0].name.toUpperCase()
-                  : post._embedded["wp:term"][1][0].name.toUpperCase()
-              }
-            />
-          );
-        })}
       </div>
     </div>
   );
